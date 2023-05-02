a23154a3a247e0cf731adb72bfe643ee8b1ae503
Oppong <amponsahoppong@gmail.com>
2023-05-02 01:10:35 +0000

    footer changes

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -7,7 +7,7 @@
 
         <div>
             <h4 class="mb-4 text-lg text-white font-Mulish">Useful Links</h4>
-            <div class="space-y-2 text-gray-400 font-Mulish">
+            <div class="space-y-2 text-sm text-gray-400 font-Mulish">
                 <p class="cursor-pointer hover:text-red-400" ><a href="/">Home</a> </p>
                 <p class="hover:text-red-400"><a href="/menu">Menu</a> </p>
                 <p class="hover:text-red-400"><a href="/contact">Contact</a> </p>
@@ -17,17 +17,17 @@
 
         <div class="text-gray-400 font-Mulish ">
             <h4 class="mb-4 text-lg text-white">Contact Info</h4>
-           <div class="space-y-2 text-gray-400 font-Mulish">
-            <p>Address: The ChowHub, Lashibi</p>
-            <p>Phone: +233 24 393 3334</p>
+           <div class="space-y-2 text-sm text-gray-400 font-Mulish">
+            <p>Address: 28 Liberty road Community 20</p>
+            <p>Phone: +233 53 062 8877 | 059 739 4742</p>
             <p>Email: chowhubfoods@gmail.com</p>
-            <p>Digital Address: GT-011-1111</p>
+            <p>Digital Address: GT-125-6043</p>
            </div>
         </div>
 
         <div>
             <h4 class="mb-4 text-lg text-white">Food Menu</h4>
-            <p class="text-gray-400 font-Mulish">Find all our delicacies on our Menu page &nbsp; <span class="text-red-400 cursor-pointer"><a href="/menu">Menu</a></span></p>
+            <p class="text-sm text-gray-400 font-Mulish">Find all our delicacies on our Menu page &nbsp; <span class="text-red-400 cursor-pointer"><a href="/menu">Menu</a></span></p>
         </div>
     </div>
 </div>
